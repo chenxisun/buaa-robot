4d92611a31942338a73ded352fae8f23b635fca5
zhangxudongtony <zhangxudongtony@didichuxing.com>
2018-04-15 20:41:24 +0800

    fix confict

--- src/main/java/cn/edu/buaa/lab/robot/service/VoiceService.java
+++ src/main/java/cn/edu/buaa/lab/robot/service/VoiceService.java
@@ -108,40 +108,40 @@ public class VoiceService {
 //        return  ;
 //    }
 
-    public int getNLPResult()
-    {
-        Match ms = new Match();
-
-        //判断是否有效
-        if (ms.isSilence())
-            return Status.SILENCE;
-
-        //判断是否是让去睡觉
-        if(ms.sleep() == Status.SLEEP)
-            return Status.SLEEP;
-
-        //判断是否是唤醒
-        if (ms.Beibei() == Status.WAKE)
-            return Status.WAKE;
-
-        //判断是否是让翻译
-        if (ms.translate())
-            return Status.TRANSLATE;
-
-        //判断是否是询问天气
-        if (ms.weather())
-            return Status.WEATHER;
-
-        //判断是否是二次应答
-        if (Status.waitNext)
-            if (ms.hasNo())
-                return Status.NO;
-            else
-                return Status.OK;
-
-        //判断是否是完整匹配
-        return ms.match();
-    }
+//    public int getNLPResult()
+//    {
+//        Match ms = new Match();
+//
+//        //判断是否有效
+//        if (ms.isSilence())
+//            return Status.SILENCE;
+//
+//        //判断是否是让去睡觉
+//        if(ms.sleep() == Status.SLEEP)
+//            return Status.SLEEP;
+//
+//        //判断是否是唤醒
+//        if (ms.Beibei() == Status.WAKE)
+//            return Status.WAKE;
+//
+//        //判断是否是让翻译
+//        if (ms.translate())
+//            return Status.TRANSLATE;
+//
+//        //判断是否是询问天气
+//        if (ms.weather())
+//            return Status.WEATHER;
+//
+//        //判断是否是二次应答
+//        if (Status.waitNext)
+//            if (ms.hasNo())
+//                return Status.NO;
+//            else
+//                return Status.OK;
+//
+//        //判断是否是完整匹配
+//        return ms.match();
+//    }
 
 //    public String getResult(String input){
 //
