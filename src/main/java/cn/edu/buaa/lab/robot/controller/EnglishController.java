98d56ca182c6f9ba65ed9f1a9f0f77f793aede2d
bjtu12301028 <jianguang.66@163.com>
2018-04-28 13:56:36 +0800

    0428-2156

--- src/main/java/cn/edu/buaa/lab/robot/controller/EnglishController.java
+++ src/main/java/cn/edu/buaa/lab/robot/controller/EnglishController.java
@@ -1,20 +1,46 @@
 package cn.edu.buaa.lab.robot.controller;
 
+import com.google.gson.Gson;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 @RequestMapping("/english")
 @RestController
 public class EnglishController {
-    @GetMapping("/get")
+    @GetMapping("/words/list")
     public ResponseEntity<?> list(@RequestParam(value = "isSleep", defaultValue = "false") final boolean isSleep
     ) throws Exception {
-//        int t = Integer.parseInt(team);
-//        Map<String, String> result = new HashMap<>();
-//        result.put("data", "hello");
-//        result.put("value", String.valueOf(t*pageNo));
-        return new ResponseEntity<>("123", HttpStatus.OK);
+//      Gson gson = new Gson();
+        Map<String, Map<String, Object>> result = new HashMap<>();
+        Map<String, String> imgUrl = new HashMap<>();
+        imgUrl.put("img1", "");
+        imgUrl.put("img2", "");
+        imgUrl.put("img3", "");
+        Map<String, Object> a = new HashMap<>();
+        a.put("wordVoice", "englishVideo的path");
+        a.put("chineseIntroduction", "A的中文解释");
+        a.put("englishVoice", "englishVideo的path");
+        a.put("englishIntroduction", "A的英文解释");
+        a.put("chineseVoice", "chineseVideo的path");
+        a.put("imgUrl", imgUrl);
+        result.put("A",a);
+
+        Map<String, Object> b = new HashMap<>();
+        a.put("wordVoice", "englishVideo的path");
+        b.put("chineseIntroduction", "B的中文解释");
+        b.put("englishVoice", "englishVideo的path");
+        b.put("englishIntroduction", "B的英文解释");
+        b.put("chineseVoice", "chineseVideo的path");
+        b.put("imgUrl", imgUrl);
+        result.put("B",b);
+
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
